03d14f6a131bc00367c9b6981dc3363533e699fc
Vera <schoenigervera@gmail.com>
2021-12-01 16:12:14 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    public Vector2 force;
+
+    private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(Vector2.up*force, ForceMode2D.Impulse);
+        }
+    }
+}
