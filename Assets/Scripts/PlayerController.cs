0c03962c61fc082c11da2999d4d5733e6f42a18d
Vera <schoenigervera@gmail.com>
2021-12-06 17:05:01 +0100

    Character controller
    - organized assets into folders
    - added movement & jump controls partially

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,40 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D myRigidbody;
-    public Vector2 force;
+    private PlayerInput playerInput;
+    private Rigidbody2D[] parts;
+
+
+    [SerializeField] private Vector2 jumpForce = new Vector2(10f, 10f);
 
     private void Awake()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void Start()
+    {
+        parts = GetComponentsInChildren<Rigidbody2D>();
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        Vector3 direction = Camera.main.ScreenToWorldPoint(playerInput.actions["Direction"].ReadValue<Vector2>());
+        direction.z = 0f;   ///no changing depth
+
+        if (playerInput.actions["Jump"].triggered)      //Add groundcheck
         {
-            myRigidbody.AddForce(Vector2.up*force, ForceMode2D.Impulse);
+            direction = Vector3.Normalize(direction - transform.position);
+            foreach (Rigidbody2D part in parts)
+            {
+                part.AddForce(jumpForce * direction, ForceMode2D.Impulse);
+            }
         }
     }
+
+    //private bool IsGrounded()
+    //{
+    //    return Physics2D.OverlapCircle()    //check for each child collider?
+    //}
 }
